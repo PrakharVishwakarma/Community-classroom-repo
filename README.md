8972142d14f2d4b4b908cfa0c63e432203382c11
PrakharVishwakarma <prakharvishwapkv180@gmail.com>
2024-07-06 00:17:19 +0530

    Modified README
    Modified Index.js

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Community-classroom-repo
 Adding line number 2
-
+Adding Line number 3 after cloning
 adding another line 
+Hellowwww!
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const sum = (a,b) =>{
-  return a+b;
+  	return a+b;
 }
 
-sumOf2And5 = sum(2,5)
+const avg = (a,b) =>{
+	return (a+b)/2;
+}
+
+sumOf2And5 = sum(2,5);
+avgOf2And5 = avg(2,5);
